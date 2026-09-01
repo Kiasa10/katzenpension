cb5f54600e3ed703160115ef64c3e854c24fe61c
Kiasa10 <145381355+Kiasa10@users.noreply.github.com>
2026-09-01 15:29:45 +0200

    Added env Render-url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5151/api',
+  production: true,
+  apiUrl: 'https://katzenpensionangularbackendaspdotnet.onrender.com/api',
 };
