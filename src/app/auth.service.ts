e8ccd76051f76efd62a405f828aedcf0f452ace0
Kiasa10 <145381355+Kiasa10@users.noreply.github.com>
2026-09-02 13:38:26 +0200

    Added logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -26,4 +26,9 @@ export class AuthService {
         catchError(() => of(false)),
       );
   }
+
+  logout() {
+    sessionStorage.removeItem('isLoggedIn');
+    this.isLoggedIn.set(false);
+  }
 }
